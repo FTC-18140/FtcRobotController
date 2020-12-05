c9c88052e8ad2ad49e9f62060d915f8d9fe87155
DESKTOP-S246I4A\beste <bestest2pnt0@gmail.com>
2020-12-04 20:00:07 -0600

    removed unecessary imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThunderbotAutoDriveByEncoder_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThunderbotAutoDriveByEncoder_Linear.java
@@ -29,18 +29,9 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import org.firstinspires.ftc.robotcore.external.Func;
-import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
-import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
