d95867bfc4f24e4fd95568b2b14ebfd66984e6c4
LAPTOP-89O3S1QA\socre <brownboys1@icloud.com>
2020-12-17 18:43:53 -0600

    Commiting Tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -18,11 +18,8 @@ public class Teleop extends LinearOpMode {
     DcMotor rightFront = null;
     DcMotor leftRear = null;
     DcMotor rightRear = null;
-    DcMotor intake ;
-    DcMotor rampMotor;
-    DcMotor shooterMotor;
-    Servo leftClaw;
-    Servo rightClaw;
+
+
 
     boolean forwardIntake;
     boolean ramp;
@@ -34,102 +31,100 @@ public class Teleop extends LinearOpMode {
 
         robot.init(hardwareMap, telemetry);
 
-        public void start () {
-        }
-
-        public void loop () {
-
-            //joystick values x and y on left stick; x only on right stick
-            double forward = gamepad1.left_stick_y;     // push left joystick forward to go forward
-            double right = -gamepad1.left_stick_x;        // push left joystick to the right to strafe right
-            double clockwise = -gamepad1.right_stick_x;   // push right joystick to the right to rotate clockwise
-
-            //Intake reverse
-
-            intake.setPower(1);
-
-            if (gamepad1.x && !forwardIntake) {
-                if (intake.getPower() == 1) intake.setPower(-1);
-                else intake.setPower(1);
-                forwardIntake = true;
-            } else if (!gamepad1.x) forwardIntake = false;
 
 
-            //Ramp motor toggle switch
+        robot.start();
 
+        robot.loop();
 
-            rampMotor.setPower(0.3);
+        //joystick values x and y on left stick; x only on right stick
+        double forward = gamepad1.left_stick_y;     // push left joystick forward to go forward
+        double right = -gamepad1.left_stick_x;        // push left joystick to the right to strafe right
+        double clockwise = -gamepad1.right_stick_x;   // push right joystick to the right to rotate clockwise
 
-            if (gamepad1.b && !ramp) {
-                if (rampMotor.getPower() == 0.3) rampMotor.setPower(0);
-                else rampMotor.setPower(0.3);
-                ramp = true;
-            } else if (!gamepad1.b) ramp = false;
-
-
-            // shooter motor toggle switch
-
-            shooterMotor.setPower(0);
-
-            if (gamepad1.a && !shooter) {
-                if (shooterMotor.getPower() == 0) shooterMotor.setPower(1);
-                else shooterMotor.setPower(0);
-                shooter = true;
-            } else if (!gamepad1.a) shooter = false;
-
-
-            //arm claw controls
-
-            if (gamepad1.right_bumper) {
-                leftClaw.setPosition(.5);
-                rightClaw.setPosition(.5);
-            } else {
-                leftClaw.setPosition(0);
-                rightClaw.setPosition(1);
-            }
-
-
-            //inverse kinematic transformation
+        //inverse kinematic transformation
 // to convert your joystick inputs to 4 motor commands:
-            double mfrontLeft = forward + clockwise + right;
-            double mfrontRight = forward - clockwise - right;
-            double mbackLeft = forward + clockwise - right;
-            double mbackRight = forward - clockwise + right;
+        double mfrontLeft = forward + clockwise + right;
+        double mfrontRight = forward - clockwise - right;
+        double mbackLeft = forward + clockwise - right;
+        double mbackRight = forward - clockwise + right;
 
 
-            //wheel speed commands
-// so that no wheel speed command exceeds magnitude of .5:
-            double max = abs(mfrontLeft);
-            if (abs(mfrontRight) > max) {
-                max = abs(mfrontRight);
-            }
-            if (abs(mbackLeft) > max) {
-                max = abs(mbackLeft);
-            }
-            if (abs(mbackRight) > max) {
-                max = abs(mbackRight);
-            }
-            if (max > 0.5) {
-                mfrontLeft /= max;
-                mfrontRight /= max;
-                mbackLeft /= max;
-                mbackRight /= max;
-            }
-            rightFront.setPower(mfrontRight);
-            rightRear.setPower(mbackRight);
-            leftFront.setPower(mfrontLeft);
-            leftRear.setPower(mbackLeft);
+        //wheel speed commands
+// so that no wheel speed command exceeds magnitude of 1:
+        double max = abs(mfrontLeft);
+        if (abs(mfrontRight) > max) {
+            max = abs(mfrontRight);
+        }
+        if (abs(mbackLeft) > max) {
+            max = abs(mbackLeft);
+        }
+        if (abs(mbackRight) > max) {
+            max = abs(mbackRight);
+        }
+        if (max > 0.5) {
+            mfrontLeft/=max;
+            mfrontRight/=max;
+            mbackLeft/=max;
+            mbackRight/=max;
+        }
+        rightFront.setPower(mfrontRight);
+        rightRear.setPower(mbackRight);
+        leftFront.setPower(mfrontLeft);
+        leftRear.setPower(mbackLeft);
 
+        //Intake reverse
+
+
+        if (gamepad1.x && !forwardIntake) {
+            if (robot.intake.getPower() == 1) robot.intake.setPower(-1);
+            else robot.intake.setPower(1);
+            forwardIntake = true;
+        } else if (!gamepad1.x) forwardIntake = false;
+
+
+        //Ramp motor toggle switch
+
+
+        robot.rampMotor.setPower(0.3);
+
+        if (gamepad1.b && !ramp) {
+            if (robot.rampMotor.getPower() == 0.3) robot.rampMotor.setPower(0);
+            else robot.rampMotor.setPower(0.3);
+            ramp = true;
+        } else if (!gamepad1.b) ramp = false;
+
+
+        // shooter motor toggle switch
+
+       robot.shooterMotor.setPower(0);
+
+        if (gamepad1.a && !shooter) {
+            if (robot.shooterMotor.getPower() == 0) robot.shooterMotor.setPower(1);
+            else robot.shooterMotor.setPower(0);
+            shooter = true;
+        } else if (!gamepad1.a) shooter = false;
+
+
+        //arm claw controls
+
+        if (gamepad1.right_bumper) {
+            robot.leftClaw.setPosition(.5);
+            robot.rightClaw.setPosition(.5);
+        } else {
+            robot.leftClaw.setPosition(0);
+            robot.rightClaw.setPosition(1);
         }
 
 
-        public void stop ()
-        {
-            rightFront.setPower(0.0);
-            leftFront.setPower(0.0);
-            rightRear.setPower(0.0);
-            leftRear.setPower(0.0);
-        }
+
+
+        robot.stop();
+
+        
 
     }
+
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Thunderbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Thunderbot.java
@@ -42,6 +42,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import static java.lang.Math.abs;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
 
 /**
@@ -64,6 +65,10 @@ public class Thunderbot
     DcMotor shooterMotor;
     Servo leftClaw;
     Servo rightClaw;
+
+    boolean forwardIntake;
+    boolean ramp;
+    boolean shooter;
 
     static final double     COUNTS_PER_MOTOR_REV    = 28;      // goBuilda 5202 motors
     static final double     DRIVE_GEAR_REDUCTION    = 19.2;    // This is < 1.0 if geared UP
@@ -135,7 +140,93 @@ public class Thunderbot
         telemetry.addData("Status", "Thunderbot Ready");
         telemetry.update();
     }
-    
+    public void start(){
+
+    }
+
+    public void loop(){
+        //joystick values x and y on left stick; x only on right stick
+        double forward = gamepad1.left_stick_y;     // push left joystick forward to go forward
+        double right = -gamepad1.left_stick_x;        // push left joystick to the right to strafe right
+        double clockwise = -gamepad1.right_stick_x;   // push right joystick to the right to rotate clockwise
+
+        //Intake reverse
+
+
+        if (gamepad1.x && !forwardIntake) {
+            if (intake.getPower() == 1) intake.setPower(-1);
+            else intake.setPower(1);
+            forwardIntake = true;
+        } else if (!gamepad1.x) forwardIntake = false;
+
+
+        //Ramp motor toggle switch
+
+
+        rampMotor.setPower(0.3);
+
+        if (gamepad1.b && !ramp) {
+            if (rampMotor.getPower() == 0.3) rampMotor.setPower(0);
+            else rampMotor.setPower(0.3);
+            ramp = true;
+        } else if (!gamepad1.b) ramp = false;
+
+
+        // shooter motor toggle switch
+
+        shooterMotor.setPower(0);
+
+        if (gamepad1.a && !shooter) {
+            if (shooterMotor.getPower() == 0) shooterMotor.setPower(1);
+            else shooterMotor.setPower(0);
+            shooter = true;
+        } else if (!gamepad1.a) shooter = false;
+
+
+        //arm claw controls
+
+        if (gamepad1.right_bumper) {
+            leftClaw.setPosition(.5);
+            rightClaw.setPosition(.5);
+        } else {
+            leftClaw.setPosition(0);
+            rightClaw.setPosition(1);
+        }
+
+
+        //inverse kinematic transformation
+// to convert your joystick inputs to 4 motor commands:
+        double mfrontLeft = forward + clockwise + right;
+        double mfrontRight = forward - clockwise - right;
+        double mbackLeft = forward + clockwise - right;
+        double mbackRight = forward - clockwise + right;
+
+
+        //wheel speed commands
+// so that no wheel speed command exceeds magnitude of .5:
+        double max = abs(mfrontLeft);
+        if (abs(mfrontRight) > max) {
+            max = abs(mfrontRight);
+        }
+        if (abs(mbackLeft) > max) {
+            max = abs(mbackLeft);
+        }
+        if (abs(mbackRight) > max) {
+            max = abs(mbackRight);
+        }
+        if (max > 0.5) {
+            mfrontLeft /= max;
+            mfrontRight /= max;
+            mbackLeft /= max;
+            mbackRight /= max;
+        }
+        rightFront.setPower(mfrontRight);
+        rightRear.setPower(mbackRight);
+        leftFront.setPower(mfrontLeft);
+        leftRear.setPower(mbackLeft);
+
+    }
+
     //method for Driving from a Current Position to a Requested Position
     public void driveStraight(double speed, double distance, double timeoutS, LinearOpMode caller)
     {
